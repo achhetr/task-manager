c9e4ccdcebbbed6eae0122159301582352985148
Akash Chhetri <akash.chhetri1@gmail.com>
2020-11-21 09:22:50 +1100

    GET method read user and read task

--- src/index.js
+++ src/index.js
@@ -14,6 +14,27 @@ app.post('/users', (req, res) => {
 
 	user.save()
 		.then((result) => {
+			res.status(201).send(user);
+		})
+		.catch((error) => {
+			res.status(400).send(error);
+		});
+});
+
+app.get('/users', (req, res) => {
+	User.find({})
+		.then((users) => {
+			res.send(users);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
+
+app.get('/users/:id', (req, res) => {
+	User.findById(req.params.id)
+		.then((user) => {
+			if (!user) return res.status(404).send('User not found');
 			res.send(user);
 		})
 		.catch((error) => {
@@ -26,6 +47,27 @@ app.post('/tasks', (req, res) => {
 
 	task.save()
 		.then((result) => {
+			res.status(201).send(task);
+		})
+		.catch((error) => {
+			res.status(400).send(error);
+		});
+});
+
+app.get('/tasks', (req, res) => {
+	Task.find({})
+		.then((tasks) => {
+			res.send(tasks);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
+
+app.get('/tasks/:id', (req, res) => {
+	Task.findById(req.params.id)
+		.then((task) => {
+			if (!task) return res.status(404).send('Task not found');
 			res.send(task);
 		})
 		.catch((error) => {
