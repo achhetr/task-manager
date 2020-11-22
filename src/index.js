234888faa3c3728e7a487613a89d3d14151bf577
Akash Chhetri <akash.chhetri1@gmail.com>
2020-11-22 11:47:27 +1100

    async await added

--- src/index.js
+++ src/index.js
@@ -9,70 +9,63 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 	const user = new User(req.body);
-
-	user.save()
-		.then((result) => {
-			res.status(201).send(user);
-		})
-		.catch((error) => {
-			res.status(400).send(error);
-		});
+	try {
+		await user.save();
+		res.status(201).send(user);
+	} catch (error) {
+		res.status(400).send(error);
+	}
 });
 
-app.get('/users', (req, res) => {
-	User.find({})
-		.then((users) => {
-			res.send(users);
-		})
-		.catch((error) => {
-			res.status(500).send(error);
-		});
+app.get('/users', async (req, res) => {
+	try {
+		const users = await User.find({});
+		res.send(users);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
-app.get('/users/:id', (req, res) => {
-	User.findById(req.params.id)
-		.then((user) => {
-			if (!user) return res.status(404).send('User not found');
-			res.send(user);
-		})
-		.catch((error) => {
-			res.status(500).send(error);
-		});
+app.get('/users/:id', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+
+		if (!user) return res.status(404).send('User not found');
+		res.send(user);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
 	const task = new Task(req.body);
-
-	task.save()
-		.then((result) => {
-			res.status(201).send(task);
-		})
-		.catch((error) => {
-			res.status(400).send(error);
-		});
+	try {
+		await task.save();
+		res.status(201).send(task);
+	} catch (error) {
+		res.status(400).send(error);
+	}
 });
 
-app.get('/tasks', (req, res) => {
-	Task.find({})
-		.then((tasks) => {
-			res.send(tasks);
-		})
-		.catch((error) => {
-			res.status(500).send(error);
-		});
+app.get('/tasks', async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.send(tasks);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
-app.get('/tasks/:id', (req, res) => {
-	Task.findById(req.params.id)
-		.then((task) => {
-			if (!task) return res.status(404).send('Task not found');
-			res.send(task);
-		})
-		.catch((error) => {
-			res.status(500).send(error);
-		});
+app.get('/tasks/:id', async (req, res) => {
+	try {
+		const task = await Task.findById(req.params.id);
+		if (!task) return res.status(404).send('Task not found');
+		res.send(task);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
