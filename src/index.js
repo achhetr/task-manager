f793583190c1536d6a452600f28f63fdf7e66e8d
Akash Chhetri <akash.chhetri1@gmail.com>
2020-11-23 08:13:07 +1100

    patch request added

--- src/index.js
+++ src/index.js
@@ -39,6 +39,30 @@ app.get('/users/:id', async (req, res) => {
 	}
 });
 
+app.patch('/users/:id', async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['name', 'email', 'password', 'age'];
+	const isValidOperation = updates.every((update) =>
+		allowedUpdates.includes(update)
+	);
+
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates' });
+	}
+
+	try {
+		const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		if (!user) return res.status(404).send('User does not exists');
+
+		res.send(user);
+	} catch (error) {
+		res.status(400).send(error);
+	}
+});
+
 app.post('/tasks', async (req, res) => {
 	const task = new Task(req.body);
 	try {
@@ -65,6 +89,29 @@ app.get('/tasks/:id', async (req, res) => {
 		res.send(task);
 	} catch (error) {
 		res.status(500).send(error);
+	}
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+	const allowedUpdates = ['description', 'completed'];
+	const updates = Object.keys(req.body);
+	const isValidOperation = updates.every((update) =>
+		allowedUpdates.includes(update)
+	);
+
+	if (!isValidOperation) return res.status(404).send('Invalid updates');
+
+	try {
+		const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!task) return res.status(404).send('Task not found');
+
+		res.send(task);
+	} catch (error) {
+		res.status(400).send(error);
 	}
 });
 
