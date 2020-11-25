04256c85e446bd519f4c7a05ff379f82379082bb
Akash Chhetri <akash.chhetri1@gmail.com>
2020-11-26 08:09:40 +1100

    delete request added

--- src/index.js
+++ src/index.js
@@ -63,6 +63,17 @@ app.patch('/users/:id', async (req, res) => {
 	}
 });
 
+app.delete('/users/:id', async (req, res) => {
+	try {
+		const user = await User.findByIdAndDelete(req.params.id);
+		if (!user) return res.status(404).send('User does not exists');
+
+		res.send(user);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
 app.post('/tasks', async (req, res) => {
 	const task = new Task(req.body);
 	try {
@@ -112,6 +123,18 @@ app.patch('/tasks/:id', async (req, res) => {
 		res.send(task);
 	} catch (error) {
 		res.status(400).send(error);
+	}
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+	try {
+		const task = await Task.findByIdAndDelete(req.params.id);
+
+		if (!task) return res.status(404).send('Task not found');
+
+		res.send(task);
+	} catch (error) {
+		res.status(500).send(error);
 	}
 });
 
