38a1b042f5e031c39bf9759cf02d8f139419464a
Akash Chhetri <akash.chhetri1@gmail.com>
2020-12-09 13:55:31 +1100

    avatar uplaoded

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 
 const User = require('../models/user');
 const auth = require('../middleware/auth');
@@ -85,6 +86,14 @@ router.delete('/users/me', auth, async (req, res) => {
 	} catch (error) {
 		res.status(500).send(error);
 	}
+});
+
+const upload = multer({
+	dest: 'avatars',
+});
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+	res.send();
 });
 
 module.exports = router;
